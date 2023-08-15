249741c293fb5b4a9e200d47a67180e1d1b9adf6
armandnur10 <zxuanggg@gmail.com>
2023-08-15 13:39:36 +0700

    add gambar as a value

--- controller/gadgetController.js
+++ controller/gadgetController.js
@@ -4,17 +4,20 @@ var barang = [
     {
         nama_barang: 'Samsung Galaxy S23 Ultra',
         harga: 20000000,
-        kategori: 'Smartphone'
+        kategori: 'Smartphone',
+        gambar: 'https://images.samsung.com/is/image/samsung/p6pim/uk/feature/164370272/uk-feature-galaxy-s-535141640?$FB_TYPE_I_JPG$'
     },
     {
         nama_barang: 'Ipad Pro 2021',
         harga: 25000000,
-        kategori: 'Tablet'
+        kategori: 'Tablet',
+        gambar: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/24143275/226374_iPad_Pro_12.9_M2_2022_DSeifert_0002.jpg'
     },
     {
         nama_barang: 'Asus ROG Flow X13',
         harga: 40000000,
-        kategori: 'Laptop'
+        kategori: 'Laptop',
+        gambar: 'https://www.yangcanggih.com/wp-content/uploads/2021/05/ASUS-ROG-Flow-X13-1.jpg'
     },
 ]
 
